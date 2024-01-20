f4b5a5e48816a89374bb843a08a0a74c9364adc3
kenilJoshi <keniljoshi3@gmail.com>
2024-01-21 00:05:00 +0530

    link update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import manImage from '../../assets/aboutPageImage.jpg'
 
 function About() {
   return (
-    <div className='w-full h-screen flex justify-center items-center'>
-        <h1>About us</h1>
+    <div className='flex gap-24'>
+      <img className='w-128' src={manImage} />
+      <div className='flex flex-col justify-center pr-14'>
+        <h1 className='text-5xl font-bold text-stone-800 pb-6'>About us</h1>
+        <p className='text-lg text-stone-800'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto laboriosam odit fugit saepe beatae ipsam obcaecati harum iure exercitationem animi ipsa, recusandae quae laborum aspernatur error aliquam explicabo nobis repellat.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, explicabo. Laboriosam, corporis neque! At vel laudantium perferendis illum aliquid perspiciatis quas ab itaque dolorum quod, est sed dolorem accusamus quia?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio at ipsum numquam provident mollitia facilis et deserunt. Obcaecati, eligendi, beatae eum dicta vero quidem id, hic quae fugiat possimus culpa?
+        </p>
+      </div>
     </div>
   )
 }
